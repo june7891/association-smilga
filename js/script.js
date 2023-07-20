6c69b9c413ba260ef839cc7f31eca4e77c07f500
toma <toma.june@gmail.com>
2023-07-20 10:50:49 +0200

    responsive update

--- js/script.js
+++ js/script.js
@@ -3,4 +3,21 @@ let body = document.querySelector('body');
 
 toggle.addEventListener('click', function() {
     body.classList.toggle('show');
-})
+});
+
+
+
+
+function changeNavBg() {
+    let navBar = document.getElementById('navbar');
+    let scrollValue = window.scrollY;
+    if(scrollValue > 200) {
+        navBar.classList.add('bgColor');
+    } else {
+        navBar.classList.remove('bgColor');
+    }
+    
+
+}
+
+window.addEventListener('scroll', changeNavBg);
